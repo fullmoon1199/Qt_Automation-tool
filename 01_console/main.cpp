0e580f2f24f2fee0bd3ba9b4e6f945c94759a58c
fullmoon1199 <fullmoon@naver.com>
2024-01-03 18:30:54 +0900

    240103

---
+++ 01_console/main.cpp
@@ -0,0 +1,10 @@
+#include <QCoreApplication>
+#include <QDebug>
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    int myVal = 100;
+    qDebug() << "Hello World.";
+    qDebug("Qt First Project. My Value. %d", myVal);
+    return a.exec();
+}
